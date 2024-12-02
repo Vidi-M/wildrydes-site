4312f3c94d27fae53ecf955df12eea753b35e6ac
Vidi-M <vs221@ic.ac.uk>
2024-12-02 12:05:15 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_obVFpPvmz', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '445hvu0eb2b2d7rjvjgven2c4v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
