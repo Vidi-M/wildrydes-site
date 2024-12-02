45738537c01b62ba9602c61dc9181af3e4810115
Vidi-M <vs221@ic.ac.uk>
2024-12-02 12:58:50 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'eu-north-1_obVFpPvmz', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '445hvu0eb2b2d7rjvjgven2c4v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+         userPoolClientSecret: COGNITO_CLIENT_SECRET,
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
