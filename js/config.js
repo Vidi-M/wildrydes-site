1c6909c26a62ea363443b7106250e2c1264588d2
Vidi-M <vs221@ic.ac.uk>
2024-12-02 14:34:27 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,7 @@
-// Load environment variables from .env file
-require('dotenv').config();
-
 window._config = {
     cognito: {
         userPoolId: 'eu-north-1_ZS8ShuBVW', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '5q9vvbhm17mq2uejtn4v4u2c2j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        userPoolClientSecret: process.env.COGNITO_CLIENT_SECRET,
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
